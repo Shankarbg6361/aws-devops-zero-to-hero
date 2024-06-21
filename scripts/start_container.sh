51d0cc20893a766de41fa6829361665766e0e600
Shankarbg6361 <62064879+Shankarbg6361@users.noreply.github.com>
2024-06-21 16:41:09 +0530

    Update start_container.sh
    Update start_container.sh port no

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull shankarbg6361/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 shankarbg6361/simple-python-flask-app:latest
+docker run -d -p 6000:6000 shankarbg6361/simple-python-flask-app:latest
